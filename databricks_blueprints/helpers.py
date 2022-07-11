6204bac425542206ca39704fffd023acee0ac143
Shingai Shamu <mshamu95@gmail.com>
2022-07-11 18:01:04 +0200

    added PR changes

--- databricks_blueprints/helpers.py
+++ databricks_blueprints/helpers.py
@@ -16,12 +16,15 @@ shipyard.logs.create_artifacts_folders(artifact_subfolder_paths)
 
 class DatabricksClient(object):
     "Databricks connection object for use in running queries"
-    def __init__(self, token, instance_id):
+    def __init__(self, token, instance_url):
         self.token = token
-        self.base_url = f"https://{instance_id}/api/2.0"
+        self.base_url = f"https://{instance_url}/api/2.0"
     
     def get_headers(self):
-        return {'Authorization': f"Bearer {self.token}"}
+        return {
+            'Authorization': f"Bearer {self.token}",
+            'Content-Type': 'application/json'
+        }
     
     def get(self, endpoint, params={}):
         api_headers = self.get_headers()
@@ -40,7 +43,7 @@ class DatabricksClient(object):
         endpoint_url = self.base_url + endpoint
         response = requests.post(endpoint_url, 
                                 headers=api_headers,
-                                data=data)
+                                json=data)
         if response.status_code == 401: # invalid account token
             sys.exit(errors.EXIT_CODE_INVALID_CREDENTIALS)
         elif response.status_code == 404: # wrong instance_id
@@ -73,6 +76,13 @@ def start_cluster(token, instance_id, cluster_id):
     start_cluster_endpoint = "/clusters/start"
     payload = {"cluster_id": cluster_id}
     start_response = databricks_client.post(start_cluster_endpoint, data=payload)
+    # save response to shipyard logs
+    response_file_name = shipyard.files.combine_folder_and_file_name(
+        artifact_subfolder_paths['responses'], 
+        f'cluster_start_{cluster_id}_response.json')
+    shipyard.files.write_json_to_file(start_response.json(), response_file_name)
+    
+    # handle response data
     if start_response.status_code == requests.codes.ok:
         print(f"Cluster: {cluster_id} started successfully")
     elif start_response.status_code == 400: # Cluster in RESTARTING state
@@ -83,8 +93,3 @@ def start_cluster(token, instance_id, cluster_id):
               f"HTTP Status code: {start_response.status_code} ",
               f"and Response: {start_response.text}")
         sys.exit(errors.EXIT_CODE_CLUSTER_STATUS_ERRORED)
-    # save response to shipyard logs
-    response_file_name = shipyard.files.combine_folder_and_file_name(
-        artifact_subfolder_paths['responses'], 
-        f'cluster_start_{cluster_id}_response.json')
-    shipyard.files.write_json_to_file(start_response.json(), response_file_name)
