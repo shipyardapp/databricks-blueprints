380b8e32850afc1579ca55e256bb732bfefcfbe4
Blake Burch <blake@shipyardapp.com>
2022-07-11 12:48:01 -0500

    Small fixes to error handling with new function

--- databricks_blueprints/terminate_cluster.py
+++ databricks_blueprints/terminate_cluster.py
@@ -7,7 +7,13 @@ User Inputs:
 - terminates a single cluster.
 """
 import argparse
-from helpers import DatabricksClient
+import sys
+import shipyard_utils as shipyard
+try:
+    import errors
+    from helpers import DatabricksClient
+except BaseException:
+    from . import errors
 
 
 def get_args():
@@ -26,10 +32,56 @@ def terminate_cluster(client, cluster_id):
     """
     terminate_cluster_endpoint = "/clusters/delete"
     payload = {"cluster_id": cluster_id}
-    client.post(terminate_cluster_endpoint,
-                           data=payload)
-    print(f"Cluster termination for id: {cluster_id} in progress...")
-    
+    try:
+        termination_response = client.post(terminate_cluster_endpoint,
+                                           data=payload)
+        base_folder_name = shipyard.logs.determine_base_artifact_folder(
+            'databricks')
+        artifact_subfolder_paths = shipyard.logs.determine_artifact_subfolders(
+            base_folder_name)
+        response_file_name = shipyard.files.combine_folder_and_file_name(
+            artifact_subfolder_paths['responses'],
+            f'cluster_termination_{cluster_id}_response.json')
+        shipyard.files.write_json_to_file(
+            termination_response.json(), response_file_name)
+    except BaseException as e:
+        print(
+            f"Ran into an error while trying to terminate cluster {cluster_id}. Please check your instance ID and try again.")
+        print(e)
+        sys.exit(errors.EXIT_CODE_INVALID_INSTANCE)
+    determine_status(termination_response, cluster_id)
+
+
+def determine_status(termination_response, cluster_id):
+    if termination_response.status_code == 200:
+        print(f"Cluster termination for id: {cluster_id} has started...")
+        sys.exit(errors.EXIT_CODE_TERMINATION_SUCCESSFULLY_STARTED)
+    elif termination_response.status_code == 400:  # Cluster in RESTARTING state
+        if "does not exist" in termination_response.json()[
+                'message']:
+            print(
+                f"Cluster: {cluster_id} does not exist. Check for typos or that you have access to this cluster.")
+            sys.exit(errors.EXIT_CODE_INVALID_CLUSTER)
+        else:
+            throw_generic_error(termination_response, cluster_id)
+    elif termination_response.status_code == 403:
+        if "Invalid access token" in termination_response.json()[
+                'message']:
+            print(
+                f"The access key provided is not valid. Check for typos.")
+            sys.exit(errors.EXIT_CODE_INVALID_CREDENTIALS)
+        else:
+            throw_generic_error(termination_response, cluster_id)
+    else:
+        throw_generic_error(termination_response, cluster_id)
+
+
+def throw_generic_error(termination_response, cluster_id):
+    print(f"Failed to start Cluster: {cluster_id}",
+          f"HTTP Status code: {termination_response.status_code} ",
+          f"and Response: {termination_response.text}")
+    sys.exit(errors.EXIT_CODE_UNKNOWN_ERROR)
+
 
 def main():
     args = get_args()
