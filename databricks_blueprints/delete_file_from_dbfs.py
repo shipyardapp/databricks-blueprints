cb95c6bb4b2b35f7c5d14812da7b564c197b4451
Shingai Shamu <mshamu95@gmail.com>
2022-07-26 00:54:32 +0200

    added check delete status of file

--- databricks_blueprints/delete_file_from_dbfs.py
+++ databricks_blueprints/delete_file_from_dbfs.py
@@ -22,6 +22,22 @@ def get_args():
     args = parser.parse_args()
     return args
 
+def check_file_status(client, file_path_and_name):
+    """ Check if file exists and get the status """
+    check_endpoint = '/dbfs/get-status'
+    params = {
+        'path': file_path_and_name
+    }
+    response = client.get(check_endpoint, params=params)
+    status_code = response.status_code
+    if status_code in requests.codes.ok: # File found
+        print(f"file {file_path_and_name} still in DBFS")
+    elif status_code == 404: # File not found
+        print(f"file {file_path_and_name} is no longer in DBFS")
+    else:
+        print(f"Unknown error whilst trying to find file: {file_path_and_name}",
+              f"error: {response.text}")
+
 
 def delete_file_from_dbfs(client, file_path_and_name):
     delete_endpoint = "/dbfs/delete"
@@ -31,7 +47,8 @@ def delete_file_from_dbfs(client, file_path_and_name):
     print(f"Start delete for {file_path_and_name}")
     delete_response = client.post(delete_endpoint, data=data)
     if delete_response.status_code == requests.codes.ok:
-        print(f"DBFS File: {file_path_and_name} delete function completed successfully")
+        print(f"DBFS File: {file_path_and_name} delete function started...")
+        check_file_status(client, file_path_and_name)
     elif delete_response.status_code == 503: # Too many files left to delete
         delete_json = delete_response.json()
         error_code = delete_json['error_code']
@@ -65,7 +82,7 @@ def main():
         matching_file_names = shipyard.files.find_all_file_matches(files,
                                             re.compile(source_file_name))
         print(f'{len(matching_file_names)} files found. Preparing to delete...')
-    # create delete file path
+        # create delete file path
         for index, file_name in enumerate(matching_file_names):
             file_path_and_name = shipyard.files.combine_folder_and_file_name(source_folder_name,
                                                                    file_name)
