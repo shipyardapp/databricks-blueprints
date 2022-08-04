406cfb74537d7a92c2eed575fa76e2cac2c572b7
Blake Burch <blake@shipyardapp.com>
2022-08-04 12:03:10 -0500

    Minor updates on enumeration logic.

--- databricks_blueprints/move_file_on_dbfs.py
+++ databricks_blueprints/move_file_on_dbfs.py
@@ -10,18 +10,37 @@ try:
 except BaseException:
     from . import databricks_client
     from . import errors
-    
+
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--access-token', dest='access_token', required=True)
-    parser.add_argument('--instance-id', dest='instance_id', required=True)
-    parser.add_argument('--source-file-name', dest='source_file_name', required=True)
-    parser.add_argument('--source-folder-name', dest='source_folder_name', required=False)
-    parser.add_argument('--dest-file-name', dest='dest_file_name', required=False)
-    parser.add_argument('--dest-folder-name', dest='dest_folder_name', required=False)
-    parser.add_argument('--source-file-name-match-type', dest='source_file_name_match_type',
-                        choices={'exact_match', 'regex_match'}, required=True)
+    parser.add_argument('--instance-url', dest='instance_url', required=True)
+    parser.add_argument(
+        '--source-file-name',
+        dest='source_file_name',
+        required=True)
+    parser.add_argument(
+        '--source-folder-name',
+        dest='source_folder_name',
+        default='/FileStore/',
+        required=False)
+    parser.add_argument(
+        '--dest-file-name',
+        dest='dest_file_name',
+        required=False)
+    parser.add_argument(
+        '--dest-folder-name',
+        dest='dest_folder_name',
+        default='/FileStore/',
+        required=False)
+    parser.add_argument(
+        '--source-file-name-match-type',
+        dest='source_file_name_match_type',
+        choices={
+            'exact_match',
+            'regex_match'},
+        required=True)
     args = parser.parse_args()
     return args
 
@@ -40,21 +59,23 @@ def dbfs_mkdirs(client, directory_path):
 def dbfs_move_file(client, source_file_path, destination_file_path):
     move_endpoint = "/dbfs/move"
     payload = {
-        "source_path": source_file_path, 
+        "source_path": source_file_path,
         "destination_path": destination_file_path
-        }
+    }
     try:
         move_response = client.post(move_endpoint, data=payload)
     except Exception as e:
         # Check if incorrect url provided first
         if 'nodename nor servname provided' in str(e):
-            print("Invalid or wrong databricks instance url provided. Please check and try again")
+            print(
+                "Invalid or wrong databricks instance url provided. Please check and try again")
             sys.exit(errors.EXIT_CODE_INVALID_INSTANCE)
         else:
             print(f"Error occurred when trying move request: {e}")
             sys.exit(errors.EXIT_CODE_UNKNOWN_ERROR)
     if move_response.status_code == requests.codes.ok:
-        print(f"File: {source_file_path} moved successfully to {destination_file_path}")
+        print(
+            f"File: {source_file_path} moved successfully to {destination_file_path}")
     elif move_response.status_code == 404:
         print(f"File: {source_file_path} does not exist")
         sys.exit(errors.EXIT_CODE_DBFS_INVALID_SOURCE)
@@ -65,42 +86,39 @@ def dbfs_move_file(client, source_file_path, destination_file_path):
         )
         sys.exit(errors.EXIT_CODE_DBFS_MOVE_ERROR)
     else:
-        print(f"DBFS {source_file_path} to {destination_file_path} failed.",
-              f"response: {move_response.text} status: {move_response.status_code}")
+        print(
+            f"DBFS {source_file_path} to {destination_file_path} failed.",
+            f"response: {move_response.text} status: {move_response.status_code}")
         sys.exit(errors.EXIT_CODE_UNKNOWN_ERROR)
 
 
 def main():
     args = get_args()
     access_token = args.access_token
-    instance_id = args.instance_id
+    instance_url = args.instance_url
     source_file_name = args.source_file_name
     source_folder_name = args.source_folder_name
     dest_file_name = args.dest_file_name
     dest_folder_name = args.dest_folder_name
     source_file_name_match_type = args.source_file_name_match_type
     # create client
-    client = databricks_client.DatabricksClient(access_token, instance_id)
-    # create file paths
-    if not source_folder_name:
-        source_folder_name = '/FileStore/'
-    else:
-        # clean folder name
-        source_folder_name = '/' + shipyard.files.clean_folder_name(source_folder_name) + '/'
-        
-    if not dest_folder_name:
-        dest_folder_name = '/FileStore/'
-    else:
-        # clean folder name
-        dest_folder_name = '/' shipyard.files.clean_folder_name(dest_folder_name) + '/'
-        
+    client = databricks_client.DatabricksClient(access_token, instance_url)
+
+    # clean folder name
+    source_folder_name = '/' + \
+        shipyard.files.clean_folder_name(source_folder_name) + '/'
+
+    # clean folder name
+    dest_folder_name = '/' + \
+        shipyard.files.clean_folder_name(dest_folder_name) + '/'
+
     if not dest_file_name:
         dest_file_name = source_file_name
-        
+
     if source_file_name_match_type == 'regex_match':
         files = databricks_client.list_dbfs_files(client, source_folder_name)
-        matching_file_names = shipyard.files.find_all_file_matches(files,
-                                            re.compile(source_file_name))
+        matching_file_names = shipyard.files.find_all_file_matches(
+            files, re.compile(source_file_name))
         num_matches = len(matching_file_names)
         if num_matches == 0:
             print("No files matching regex found")
@@ -110,10 +128,16 @@ def main():
         dbfs_mkdirs(client, dest_folder_name)
         # create move file path
         for index, file_name in enumerate(matching_file_names):
+            file_number = None if num_matches == 1 else index + 1
             source_file_path = shipyard.files.combine_folder_and_file_name(
-                            source_folder_name, file_name)
-            dest_file_path = shipyard.files.combine_folder_and_file_name(
-                            dest_folder_name, f"{dest_file_name}_{index}")
+                folder_name='', file_name=file_name)
+            dest_file_path = shipyard.files.determine_destination_full_path(
+                destination_folder_name=dest_folder_name,
+                destination_file_name=dest_file_name,
+                source_full_path=source_file_path,
+                file_number=file_number)
+            # dest_file_path = shipyard.files.combine_folder_and_file_name(
+            #     dest_folder_name, f"{dest_file_name}_{index}")
             dbfs_move_file(client, source_file_path, dest_file_path)
     else:
         source_file_path = shipyard.files.combine_folder_and_file_name(
